9e527d8821d8f124f1c64780f11de51cf5350fd6
darkstar21 <mikeh09@gmail.com>
2014-09-23 12:25:37 -0700

    Implemented backbone model & collection, working on view

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,70 @@
 // YOUR CODE HERE:
-var app = {
-  messages: [],
-  rooms: {},
-  server: 'https://api.parse.com/1/classes/chatterbox',
-  mostRecent: '2011-08-20T02:06:57.931Z',
-  filters: {},
-  currentRoom: undefined,
-  init: function(){},
+
+var Message = Backbone.Model.extend({
+  initialize: function(){
+    this.view = new MessageView(this);
+    this.trigger('change');
+  }
+});
+
+var Messages = Backbone.Collection.extend({
+  model: Message
+});
+
+var messages = new Messages();
+
+var MessageView = Backbone.View.extend({
+  tagName: 'div',
+  initialize: function(model){
+    this.render(model);
+  },
+  render: function(model){
+    var $user = $('<a href="#" class="username" onclick="this.addFriend();"></a>')
+    this.$el.text(' ' + model.get('text'));
+    $user.text(model.get('username'));
+    this.$el.prepend($user);
+    $('#chats').prepend(this.$el);
+  }
+})
+
+var Room = Backbone.Model.extend({
+  initialize: function(){
+    var $room = $('<li></li>');
+    var $roomLink = $('<a href="#"></a>');
+    $roomLink.text(this.get('room'));
+    $room.append($roomLink);
+    $('#roomSelect').append($room);
+    $roomLink.click(function(e){
+      e.preventDefault();
+      app.enterRoom(this.get('room'));
+    })
+  }
+})
+
+var Rooms = Backbone.Collection.extend({
+  model: Room
+})
+
+var rooms = new Rooms();
+
+var App = Backbone.Model.extend({
+  initialize: function(){
+    this.set('rooms',{});
+    this.set('server','https://api.parse.com/1/classes/chatterbox');
+    this.set('mostRecent','2011-08-20T02:06:57.931Z');
+    this.set('filters',{});
+    this.set('currentRoom',undefined);
+  }
+});
+
+var AppMethods = {
   send: function(message){
     $.ajax({
-      url: app.server,
+      url: this.get('server'),
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
+      context: this,
       success: function (data) {
         console.log('chatterbox: Message sent');
       },
@@ -23,15 +75,16 @@ var app = {
   },
   fetch: function(){
     $.ajax({
-      url: app.server,
+      url: this.get('server'),
       type: 'GET',
+      context: this,
       contentType: 'application/json',
       data: {
         'where': JSON.stringify({
           'updatedAt': {
             '$gt': {
               '__type': 'Date',
-              'iso': app.mostRecent
+              'iso': this.get('mostRecent')
             }
           }
         }),
@@ -40,77 +93,63 @@ var app = {
       },
       success: function (data) {
         if (data.results.length > 0) {
-          app.mostRecent = data.results[0].updatedAt;
+          this.set('mostRecent', data.results[0].updatedAt);
           for (var i = data.results.length - 1; i >= 0; i--) {
-            app.messages.push(data.results[i]);
-            if (data.results[i].room){
-              app.rooms[data.results[i].room] = data.results[i].room;
+            messages.add(new Message({
+              username: data.results[i].username,
+              text: data.results[i].text,
+              room: data.results[i].room
+            }));
+            if (data.results[i].room && !rooms.findWhere({room: data.results[i].room})){
+              rooms.add(new Room({
+                room: data.results[i].room
+              }));
             }
-          };
+          }
         }
       },
       error: function (data) {
         console.error('chatterbox: Failed to fetch message');
       },
       complete: function (data) {
-        setTimeout(app.fetch, 1000);
+        setTimeout(this.fetch.bind(this), 1000);
       }
     });
   },
   clearMessages: function(){
     $('#chats').html('');
   },
-  addMessage: function(data){
-    for (var filter in app.filters) {
-      if (data[filter] !== app.filters[filter]) {
-        return;
-      }
-    }
-    var $message = $('<div></div>');
-    var $user = $('<a href="#" class="username" onclick="app.addFriend();"></a>')
-    $message.text(' ' + data.text);
-    $user.text(data.username);
-    $message.prepend($user);
-    $('#chats').prepend($message);
-  },
-  addRoom: function(room){
-    var $room = $('<li></li>');
-    var $roomLink = $('<a href="#"></a>');
-    $roomLink.text(room);
-    $room.append($roomLink);
-    $('#roomSelect').append($room);
-    $roomLink.click(function(e){
-      e.preventDefault();
-      app.enterRoom(room);
-    })
-  },
   enterRoom: function(room){
-    app.filters.room = room;
-    app.currentRoom = room;
-    app.refresh();
+    this.get('filters').room = room;
+    this.set('currentRoom', room);
+    this.refresh();
     if(room === undefined){
       $('.go-back').toggle(false);
     } else{
       $('.go-back').toggle(true);
     }
   },
-  addFriend: function(){
-    // Nothing
-  },
   handleSubmit: function(){
-    app.send({
+    this.send({
       username: $('#username').val(),
       text: $('#message').val(),
-      room: app.currentRoom
+      room: this.get('currentRoom')
     })
   },
   refresh: function(){
-    app.clearMessages();
-    for (var i = 0; i < app.messages.length; i++) {
-      app.addMessage(app.messages[i]);
+    this.clearMessages();
+    for (var i = 0; i < this.get('messages').length; i++) {
+      this.addMessage(this.get('messages')[i]);
     }
   }
-};
+}
+
+var app = new App();
+_.extend(app, AppMethods);
+
+// var appView = Backbone.View.extend({
+
+// });
 
 $(document).ready(function(){
 
@@ -130,18 +169,10 @@ $(document).ready(function(){
     app.enterRoom(undefined);
   })
 
-  Object.observe(app.messages, function(changes){
+  Object.observe(app.get('rooms'), function(changes){
     changes.forEach(function(change) {
       if (change.type === 'add') {
-        app.addMessage(app.messages[change.name]);
-      }
-    });
-  });
-
-  Object.observe(app.rooms, function(changes){
-    changes.forEach(function(change) {
-      if (change.type === 'add') {
-        app.addRoom(app.rooms[change.name]);
+        app.addRoom(app.get('rooms')[change.name]);
       }
     });
   });
